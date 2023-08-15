6c3c755c0659d12e94155ad7b8d216b317c9462c
RedoRojo <cristhiabvictorrm@gmail.com>
2023-08-14 23:25:14 -0400

    Saludo segun el idioma implementado

--- src/presenter.js
+++ src/presenter.js
@@ -6,19 +6,41 @@ const div = document.querySelector("#response-container");
 const masculino = document.querySelector("#male-gender");
 const femenino = document.querySelector("#female-gender");
 
-let saludo = "Hola! ";
 button.addEventListener("click", () => {
+
+    let saludo = "Hola! ";
     let date = new Date(); 
     let hour = date.getHours(); 
+    let userLang = navigator.language
 
-    if(hour >= 6 && hour<= 12)  saludo += "Buenos dias "; 
-    else if(hour > 12 && hour < 19) saludo += "Buenas tardes ";
-    else saludo += "Buenas noches ";
+    
+    if(userLang == "en-US") saludo = "Hello! "; 
+
+    if(hour >= 6 && hour<= 12) {
+        if(userLang == "en-US") saludo += "Good morning ";
+        else saludo += "Buenos dias "; 
+    }
+    else if(hour > 12 && hour < 19) {
+        if(userLang == "en-US") saludo += "Good afternoon ";
+        else saludo += "Buenas tardes ";
+    }
+    else {
+        if(userLang == "en-US") saludo += "Good evening ";
+        else saludo += "Buenas noches ";
+        
+    } 
 
     if(Number.parseInt(age.value) > 30) { 
-        if(masculino.checked) saludo += "Sr.  ";
-        else if(femenino.checked) saludo += "Sra.  ";
+        if(masculino.checked) {
+            if(userLang == "en-US") saludo += "Mr.  "; 
+            else saludo += "Sr.  ";
+        }
+        else if(femenino.checked) {
+            if(userLang == "en-US") saludo += "Mrs.  "; 
+            else saludo += "Sra.  ";
+        }
     }
+    
 
     div.innerHTML = "<p>" + saludo + name.value + "</p"
 }
