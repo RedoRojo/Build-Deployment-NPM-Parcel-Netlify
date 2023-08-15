6ed51d0214f208e07b44e43c7627fe471c94d8ed
RedoRojo <cristhiabvictorrm@gmail.com>
2023-08-14 22:46:10 -0400

    Saludo tomando en cuenta la edad implementado

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,6 @@
 const name = document.querySelector("#user-name");
+const age = document.querySelector("#user-age"); 
+
 const button = document.querySelector("#greeting-button");
 const div = document.querySelector("#response-container");
 const masculino = document.querySelector("#male-gender");
@@ -7,8 +9,10 @@ const femenino = document.querySelector("#female-gender");
 let saludo = "Hola! ";
 button.addEventListener("click", () => {
     
-    if(masculino.checked) saludo += "Sr.  ";
-    else if(femenino.checked) saludo += "Sra.  ";
+    if(Number.parseInt(age.value) > 30) { 
+        if(masculino.checked) saludo += "Sr.  ";
+        else if(femenino.checked) saludo += "Sra.  ";
+    }
 
     div.innerHTML = "<p>" + saludo + name.value + "</p"
 }
