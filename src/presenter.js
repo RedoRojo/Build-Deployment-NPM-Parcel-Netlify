95ebbeecf176c35d0f73357b330f540e230534a5
RedoRojo <cristhiabvictorrm@gmail.com>
2023-08-14 22:40:59 -0400

    Saludo tomando en cuenta el genero implementado

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,15 @@
 const name = document.querySelector("#user-name");
 const button = document.querySelector("#greeting-button");
 const div = document.querySelector("#response-container");
-const saludo = "Hola! ";
+const masculino = document.querySelector("#male-gender");
+const femenino = document.querySelector("#female-gender");
+
+let saludo = "Hola! ";
 button.addEventListener("click", () => {
-    console.log(name.value);
+    
+    if(masculino.checked) saludo += "Sr.  ";
+    else if(femenino.checked) saludo += "Sra.  ";
+
     div.innerHTML = "<p>" + saludo + name.value + "</p"
 }
 )
